af350d94ad3e15143ace2b5d57a00be6ac133a55
Emre Ozturk <emreeozturk42@gmail.com>
2023-11-29 19:32:08 +0300

    index page updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LoadMore from '@/components/LoadMore'
 import { data } from '@/data'
 export default function Home() {
   return (
-    <main className="">
+    <main className="py-16 px-8 sm:p-16 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
       <section className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10">
         {
