af6ef5efec484b4536cab78f3b558f6b9d91ddca
Emre Ozturk <emreeozturk42@gmail.com>
2023-11-29 19:11:14 +0300

    project init

---
+++ prop-types.d.ts
@@ -0,0 +1,14 @@
+export type AnimeCardComponentProps = {
+  anime: {
+    id: string;
+    name: string;
+    image: {
+      original: string;
+    };
+    kind: string;
+    episodes: number;
+    episodes_aired: number;
+    score: string;
+  };
+  index: number;
+};
