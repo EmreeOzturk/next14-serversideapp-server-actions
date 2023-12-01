b209229f80ee50c18d5155660443e78362f4cafa
Emre Ozturk <emreeozturk42@gmail.com>
2023-12-01 20:45:05 +0300

    animations added

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,53 +1,56 @@
 import Image from 'next/image'
 import { AnimeCardComponentProps } from '@/prop-types'
+import MotionDiv from './MotionDiv'
 const AnimeCard: React.FC<AnimeCardComponentProps> = ({
     anime,
     index
 }) => {
     return (
-        <div className='max-w-sm rounded-md relative w-full'>
-            <div className='relative w-full h-[37vh]'>
-                <Image src={'https://shikimori.one/'+anime?.image.original} fill className='rounded-xl' alt={anime.name} />
-            </div>
-            <div className='py-4 flex flex-col gap-3'>
-                <div className='flex justify-between items-center gap-1'>
-                    <h2 className='font-bold text-white text-xl line-clamp-1 w-full'>{anime.name}</h2>
-                    <div className='py-1 px-2 bg-[#161921] rounded-sm'>
-                        <p className="text-white text-sm font-bold capitalize">
-                            {anime.kind}
-                        </p>
+        <MotionDiv>
+            <div className='max-w-sm rounded-md relative w-full'>
+                <div className='relative w-full h-[37vh]'>
+                    <Image src={'https://shikimori.one/' + anime?.image.original} fill className='rounded-xl' alt={anime.name} />
+                </div>
+                <div className='py-4 flex flex-col gap-3'>
+                    <div className='flex justify-between items-center gap-1'>
+                        <h2 className='font-bold text-white text-xl line-clamp-1 w-full'>{anime.name}</h2>
+                        <div className='py-1 px-2 bg-[#161921] rounded-sm'>
+                            <p className="text-white text-sm font-bold capitalize">
+                                {anime.kind}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex gap-4 items-center">
+                        <div className="flex flex-row gap-2 items-center">
+                            <Image
+                                src="./episodes.svg"
+                                alt="episodes"
+                                width={20}
+                                height={20}
+                                className="object-contain"
+                            />
+                            <p className="text-base text-[#d03941] font-bold">
+                                {anime.episodes || anime.episodes_aired}
+                            </p>
+                        </div>
+                        <div className="flex flex-row gap-2 items-center">
+                            <Image
+                                src="./star.svg"
+                                alt="star"
+                                width={18}
+                                height={18}
+                                className="object-contain"
+                            />
+                            <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
+                        </div>
                     </div>
                 </div>
-                <div className="flex gap-4 items-center">
-                    <div className="flex flex-row gap-2 items-center">
-                        <Image
-                            src="./episodes.svg"
-                            alt="episodes"
-                            width={20}
-                            height={20}
-                            className="object-contain"
-                        />
-                        <p className="text-base text-[#d03941] font-bold">
-                            {anime.episodes || anime.episodes_aired}
-                        </p>
-                    </div>
-                    <div className="flex flex-row gap-2 items-center">
-                        <Image
-                            src="./star.svg"
-                            alt="star"
-                            width={18}
-                            height={18}
-                            className="object-contain"
-                        />
-                        <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
-                    </div>
+                <div>
+                    <div></div>
+                    <div></div>
                 </div>
             </div>
-            <div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
+        </MotionDiv>
     )
 }
 
